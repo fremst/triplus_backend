92553ab9f5315b736fe18ebd0303564c1fdb247d
Chankyu Kim <chkiya@gmail.com>
2022-08-26 16:01:04 +0900

    etc-백업용 임시 커밋

--- src/main/java/com/triplus/util/controller/BlobUploadController.java
+++ src/main/java/com/triplus/util/controller/BlobUploadController.java
@@ -1,5 +1,8 @@
 package com.triplus.util.controller;
 
+import com.triplus.board.dto.PkgImgDto;
+import com.triplus.board.mapper.PkgImgMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +19,9 @@ import java.util.HashMap;
 @RestController
 public class BlobUploadController {
 
+    @Autowired
+    PkgImgMapper pkgImgMapper;
+
     // 파일을 DB에 blob 형식으로 저장하는 컨트롤러
     @PostMapping(value = "/blobupload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
     public HashMap<String, String> insert(ArrayList<MultipartFile> files) {
@@ -29,6 +35,11 @@ public class BlobUploadController {
                 byte[] blobFile = file.getBytes();
 
                 /* 여기에 DB 저장 코드 추가 */
+                pkgImgMapper.insert(
+                        new PkgImgDto(0,
+                                100,
+                                blobFile)
+                );
 
                 result.put("result", "success");
 
