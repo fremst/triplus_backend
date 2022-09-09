4d51a6956303cfcb2109ff68a1c7c299bcd60eab
Chankyu Kim <chkiya@gmail.com>
2022-09-09 17:31:51 +0900

    테스트 관련 파일 삭제
    - 테스트 DTO, Mapper, Service, Test 삭제

--- src/main/java/com/triplus/test/mapper/TestMapper.java
+++
@@ -1,9 +0,0 @@
-package com.triplus.test.mapper;
-
-import com.triplus.test.dto.TestDto;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TestMapper {
-    int insert(TestDto dto);
-}
